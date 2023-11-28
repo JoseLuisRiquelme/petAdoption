60514b27f0ddf9ed3d000a2a12b7f3003657259c
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-11-28 14:29:40 -0300

    Actualizar

--- petAdoption/src/main.jsx
+++ petAdoption/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Banner from './components/Banner.jsx'
-import Register from './context/Register.jsx'
+import Register from './views/Register.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Banner />
 
-    <Register />    
+    <Register />
   </React.StrictMode>,
 )
