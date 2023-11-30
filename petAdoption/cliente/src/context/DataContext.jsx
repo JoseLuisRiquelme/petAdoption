0ebcf971bf747def64d91013a6c73a498a42f86d
Jose L. Riquelme <jlrsaez@gmail.com>
2023-11-30 10:03:12 -0300

    borrando carpeta de modulos subidas al parecer erroneamente

--- petAdoption/cliente/src/context/DataContext.jsx
+++ petAdoption/cliente/src/context/DataContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState, useContext } from "react";
 import axios from 'axios';
 
+
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
@@ -10,20 +11,20 @@ export const DataProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const getData = async () => {
+    const getData =async () => {
         try {
-            const response = await axios.get(url);
-            setMascotas(response.data);
-            console.log(mascotas)
+          const response = await fetch('./mascotas.json');
+  
+          if (!response.ok) {
+            throw new Error('No se pudieron obtener los datos');
+          }
+  
+          const data = await response.json();
+          setMascotas(data);
         } catch (error) {
-            console.error('Error al obtener las mascotas:', error);
-            setError('Hubo un error al cargar las mascotas. Por favor, inténtalo de nuevo.');
-            console.log(error)
-        } finally {
-            setLoading(false);
+          console.error('Error al obtener datos:', error.message);
         }
-    };
-
+      }
     useEffect(() => {
         getData();
     }, []);
