cbe90a62d5240da455ad0049846db7304518f38b
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-12-05 15:56:14 -0300

    Muro de la Fama

--- petAdoption/cliente/src/components/Banner.jsx
+++ petAdoption/cliente/src/components/Banner.jsx
@@ -1,12 +1,13 @@
 import { PropTypes } from "prop-types";
 
-const Banner = ({title}) => {
+const Banner = ({title, subtitle}) => {
     return (
-        <div className="container py-5">
+        <div className="container mb-10">
             <div className="columns-1">
-                <div className="banner rounded p-5 relative">
-                    <h2 className="banner__title text-white font-semibold text-base md:text-2xl lg:text-4xl xl:text-5xl">{title}</h2>
-                    <img src="./src/assets/image/banner.png" alt="banner" className="banner__image absolute bottom-0 left-0" />
+                <div className="banner rounded-b p-5 relative">
+                    <h2 className="banner__title text-white font-semibold text-base text-center md:text-2xl lg:text-4xl xl:text-5xl">{title}</h2>
+                    <h3 className="banner__subtitle  text-white font-semibold text-base text-center">{subtitle}</h3>
+                    <img src="./src/assets/banner.png" alt="banner" className="banner__image absolute bottom-0 left-0" />
                 </div>
             </div>
         </div>        
@@ -17,8 +18,5 @@ export default Banner;
 
 Banner.propTypes = {
     title: PropTypes.string.isRequired,
-}
-
-Banner.defaultProps = {
-    title: 'Crear Cuenta',
+    subtitle: PropTypes.string,
 }
