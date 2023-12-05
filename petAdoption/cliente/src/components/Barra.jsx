14834f84eadc6533ef427b924dab6bee03dc70aa
Jose L. Riquelme <jlrsaez@gmail.com>
2023-12-04 23:03:06 -0300

    resolviendo conflictos PR

---
+++ petAdoption/cliente/src/components/Barra.jsx
@@ -0,0 +1,33 @@
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
+
+
+const NavBar = () => {
+  const navbarStyle = {
+    background: 'linear-gradient(to right, #ff5757, #8c52ff)'
+    
+  };
+  return (
+    <div style={navbarStyle}>
+      <Navbar    className="barraInfo">
+        <Container>
+          <NavLink className={({ isActive }) => (isActive ? "active" : undefined)} to="/" >
+            <Navbar.Brand style={{ color: "white" }}>
+            <i className= 'fa-solid fa-paw' style={{color: '#fafafa;'}}></i> PETCONNECT
+            </Navbar.Brand>
+          </NavLink>
+          <Nav className="me-3">
+            <NavLink to="/Perfil">
+              <Navbar.Brand className={({ isActive }) => (isActive ? "active" : undefined)} style={{ color: "white" }}> Perfil </Navbar.Brand>
+            </NavLink>
+            <NavLink to="/Registro">
+              <Navbar.Brand className={({ isActive }) => (isActive ? "active" : undefined)} style={{ color: "white" }}> Registrar </Navbar.Brand>
+            </NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
