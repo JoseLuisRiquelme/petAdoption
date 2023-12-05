23c34dd910fec6c5a1f8c7883ebeaa761809b70e
Jose L. Riquelme <jlrsaez@gmail.com>
2023-12-04 22:21:21 -0300

    agregando un usuarios de pruebaa la base de datos local

--- petAdoption/servidor/petAdoption.sql
+++ petAdoption/servidor/petAdoption.sql
@@ -4,11 +4,11 @@ CREATE DATABASE petAdoptionDB;
 \c petAdoptionDB
 
 -- Crea tabla Usuarios
-CREATE TABLE Usuarios (id SERIAL PRIMARY KEY,nombre VARCHAR(100),imagen VARCHAR(250), email VARCHAR(100),contrasena VARCHAR(100),direccion VARCHAR(200),ciudad VARCHAR(50),telefono VARCHAR(20),fecha_registro TIMESTAMP
+CREATE TABLE Usuarios (id SERIAL PRIMARY KEY,nombre VARCHAR(100),imagen VARCHAR(250), email VARCHAR(100),contrasena VARCHAR(100),direccion VARCHAR(200),ciudad VARCHAR(50),telefono INT,fecha_registro TIMESTAMP
 );
 
 -- Crea tabla Mascotas
-CREATE TABLE Mascotas (id SERIAL PRIMARY KEY,id_usuario INT REFERENCES Usuarios(id),nombre VARCHAR(100),imagen TEXT,especie VARCHAR(100),descripcion TEXT,ciudad VARCHAR(200),sexo VARCHAR(100), edad INT,fecha_publicacion TIMESTAMP
+CREATE TABLE Mascotas (id SERIAL PRIMARY KEY,id_usuarios INT,FOREIGN KEY (id_usuarios) REFERENCES Usuarios(id),nombre VARCHAR(100),imagen TEXT,especie VARCHAR(100),descripcion TEXT,ciudad VARCHAR(200),sexo VARCHAR(100), edad INT,fecha_publicacion TIMESTAMP
 );
 
 -- Crea tabla de Favoritos
@@ -19,3 +19,8 @@ CREATE TABLE Adoptantes (id SERIAL PRIMARY KEY,id_usuarios INT,id_mascotas INT,F
 
 -- Crea tabla de Adoptados
 CREATE TABLE Adoptados (id SERIAL PRIMARY KEY,id_usuarios INT,id_mascotas INT,FOREIGN KEY (id_usuarios) REFERENCES usuarios(id),FOREIGN KEY (id_mascotas) REFERENCES mascotas(id));
+
+--Insertar usuarios de prueba
+INSERT INTO usuarios (nombre, imagen, email, contrasena, direccion, ciudad, telefono) 
+VALUES ('Narumi', 'Imagen1', 'EmailNarumi@correo.cl', 'pass', 'su calle', 'Kioto', 555563);
+select * from usuarios;
