115a1b7b20be972cc3332e17f6cba67798ddeb07
Jose L. Riquelme <jlrsaez@gmail.com>
2023-12-07 07:20:58 -0300

    modificando main.js

--- petAdoption/cliente/src/main.jsx
+++ petAdoption/cliente/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
+import App from './App.jsx'
 //import './bootstrap/dist/css/bootstrap.min.css'
 import '../index.css'
 import Banner from './components/Banner'
@@ -8,19 +8,14 @@ import Banner from './components/Banner'
 import Register from './views/Register'
 //import Register from './views/Register'
 //import { Wall } from './views/Wall'
-//import { DataProvider } from './context/DataContext.jsx'
+import { DataProvider } from './context/DataContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <Banner title="Crear Cuenta" subtitle="" />
-
-    <Register />
-    
-    {/*
     <DataProvider>
       <App />
     </DataProvider>
-    */}
+    
   </React.StrictMode>,
 )
